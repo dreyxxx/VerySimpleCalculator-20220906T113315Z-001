9300210f14dc1dbd46b7259b6e20b66cef64b82d
kenta23 <dreyx0159@gmail.com>
2022-09-08 10:18:34 +0800

    changes saved

--- VerySimpleCalculator/VerySimpleCalculator/Form1.vb
+++ VerySimpleCalculator/VerySimpleCalculator/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
-    Dim firstNum, secondNum, sum, product, quotient As Double
+    Dim firstNum, secondNum, sum, product, quotient As Double 'naka provide na variable guys eto lang gamitin sa codes nyo
 
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
