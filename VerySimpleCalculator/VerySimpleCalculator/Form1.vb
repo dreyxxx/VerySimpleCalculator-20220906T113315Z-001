0915687bbe85c417acba81698e00023f1a53ffed
kenta23 <dreyx0159@gmail.com>
2022-09-09 10:15:21 +0800

    changes made

--- VerySimpleCalculator/VerySimpleCalculator/Form1.vb
+++ VerySimpleCalculator/VerySimpleCalculator/Form1.vb
@@ -8,7 +8,7 @@
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnProduct.Click
